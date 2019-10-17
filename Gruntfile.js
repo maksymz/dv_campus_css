0199dc00861101872a5b0475bb67a3fa027ccdd9
Maksym Zapoorozhets <maksimzaporozhets@gmail.com>
2019-10-18 00:49:31 +0300

    Added autoprefixer

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,14 +9,30 @@ module.exports = function(grunt) {
             target: {
                 files: {
                     'pub/css/responsive-styles.min.css': [
-                        'assets/dist/css/responsive-styles.css'
+                        'assets/dist/css/responsive-styles-processed.css'
                     ]
                 }
             }
-        }
+        },
+
+        postcss: {
+            options: {
+                processors: [
+                    require('autoprefixer')({
+                        overrideBrowserslist: ['last 2 versions',  'ie 11']
+                    })
+                ]
+            },
+            dist: {
+                src: 'assets/dist/css/responsive-styles.css',
+                dest: 'assets/dist/css/responsive-styles-processed.css'
+            }
+        },
+
     });
 
+    grunt.loadNpmTasks('grunt-postcss');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-    grunt.registerTask('default', ['cssmin']);
+    grunt.registerTask('default', ['postcss', 'cssmin']);
 };
