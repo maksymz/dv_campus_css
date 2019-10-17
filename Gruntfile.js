309a12411cb209c439d1af925f2320303246eca1
Maksym Zapoorozhets <maksimzaporozhets@gmail.com>
2019-10-18 00:33:49 +0300

    Added CSS minification

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,22 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-
+        cssmin: {
+            options: {
+                sourceMap: true,
+                mergeIntoShorthands: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'pub/css/responsive-styles.min.css': [
+                        'assets/dist/css/responsive-styles.css'
+                    ]
+                }
+            }
+        }
     });
 
-    grunt.registerTask('default', []);
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('default', ['cssmin']);
 };
